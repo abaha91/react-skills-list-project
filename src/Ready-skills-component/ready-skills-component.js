678660dc0269ff9092f7ffa9b291537adc1f8443
Gennadiy <g.abashkin@petrovich.ru>
2019-02-01 16:59:43 +0300

    App created

---
+++ src/Ready-skills-component/ready-skills-component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './ready-skills-component.css';
+
+class ReadySkills extends Component {
+  render() {
+    return (
+      <div className="ready-skills">
+
+      </div>
+  );
+  }
+}
+
+export default ReadySkills;
---
+++ src/Todo-skills-component/todo-skills-component.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './todo-skills-component.css';
+
+class TodoSkills extends Component {
+  render() {
+    return (
+      <div className="todo-skills"></div>
+    );
+  }
+}
+
+export default TodoSkills;
