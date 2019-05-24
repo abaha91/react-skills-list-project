71b05ca02f777e7f8948d3556a4a3a169b565b0d
Gennadiy <g.abashkin@petrovich.ru>
2019-05-24 15:14:15 +0300

    on click adding new skill

--- src/New-skill-component/new-skill-component.js
+++ src/New-skill-component/new-skill-component.js
@@ -8,21 +8,28 @@ class NewSkill extends Component {
     super(props);
   }
 
-  onChangeFunction(event) {
-    event.preventDefault();
-    this.props.dispatch(changeValue(event.target.value))
+  onKeyDownFunction(event) {
+    if (event.key == 'Enter') {
+      event.preventDefault();
+      if (event.target.value) {
+        this.props.dispatch(toAddSkill([...this.props.skills, event.target.value]));
+        event.target.value = '';
+      }
+    } else {
+      this.props.dispatch(changeValue(event.target.value));
+    }
   }
 
   toAddSkill(event) {
     event.preventDefault();
-    this.props.dispatch(toAddSkill([...this.props.skills, this.props.inputValue]))
+    this.props.dispatch(toAddSkill([...this.props.skills, this.props.inputValue]));
   }
 
   render() {
     return (
       <div className="new-skill">
         <form className="new-skill--form">
-          <input type="text" className="new-skill--input" placeholder="Введите новую цель" value={this.props.inputValue} onChange={e => this.onChangeFunction(e)}/>
+          <input type="text" className="new-skill--input" placeholder="Введите новую цель" onKeyDown={e => this.onKeyDownFunction(e)}/>
           <input type="button" className="new-skill--submit" value="Новая цель" onClick={ e => {this.toAddSkill(e)}}/>
         </form>
       </div>
